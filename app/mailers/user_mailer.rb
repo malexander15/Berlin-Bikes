b4941296309b5f160c28609b382da9489c05ac02
Maximilian Alexander <creepinjezus15@gmail.com>
2016-07-06 00:50:03 +0000

    5.3 thank you styles

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
         :to => 'creepinjezus15@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+  
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+end
 end
