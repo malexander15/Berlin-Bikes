e6425609feb617d37eeacae10458aca467334c74
Maximilian Alexander <creepinjezus15@gmail.com>
2016-07-15 21:25:59 +0000

    Fix for devise and cancan

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
   
+ devise_for :users
+	resources :products do
+		resources :comments
+	end
   resources :users
-  resources :products do
-    resources :comments
-  end
   
 
   get 'static_pages/about'
