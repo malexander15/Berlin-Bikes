f537783dc19f963cab897bbceed157a68a077c09
Maximilian Alexander <creepinjezus15@gmail.com>
2016-08-02 22:51:05 +0000

    6.13 add old code back

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,6 @@
 var refreshRating = function() {
-	$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets/images',
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets',
 		readOnly: true,
 		score: function() {
 			return $(this).attr('data-score');
@@ -11,5 +11,5 @@ var refreshRating = function() {
 $(document).on('turbolinks:load', function() {
 	refreshRating();
 
-	$('.img-zoom').elevateZoom();
+	$('.img-zoom').elevateZoom({scrollZoom : true});
 });
