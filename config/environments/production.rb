cb43bc4b9dcf799968fd7230e924c035e664ccde
Maximilian Alexander <creepinjezus15@gmail.com>
2016-08-02 03:13:14 +0000

    6.13 try adding broadcasting to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.web_socket_server_url = "wss://https://berlinbikesdemo.herokuapp.com/.herokuapp.com/cable"
+config.action_cable.allowed_request_origins = ['https://https://berlinbikesdemo.herokuapp.com/.herokuapp.com', 'http://https://berlinbikesdemo.herokuapp.com/.herokuapp.com']
 end
