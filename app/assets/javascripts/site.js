9f447c72b64179e070e5c5a0d5995561f3efe6fd
Maximilian Alexander <creepinjezus15@gmail.com>
2016-08-02 03:04:03 +0000

    6.13 potential raty star fix in heroku

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,15 @@
+var refreshRating = function() {
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets',
+		readOnly: true,
+		score: function() {
+			return $(this).attr('data-score');
+		}
+	});
+};
+
 $(document).on('turbolinks:load', function() {
+	refreshRating();
 
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
-    
-    $('.img-zoom').elevateZoom();
+	$('.img-zoom').elevateZoom();
 });
-
